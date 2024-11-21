b3d945679ea15e9fdb9e75bd7c2b61b8dbdd82f9
spoonmilk <haku31229@gmail.com>
2024-11-20 23:28:49 -0500

    added stuff

--- init.lua
+++ init.lua
@@ -70,7 +70,7 @@ Kickstart Guide:
     These are hints about where to find more information about the relevant settings,
     plugins or Neovim features used in Kickstart.
 
-   NOTE: Look for lines like this
+   NOTE Look for lines like this
 
     Throughout the file. These are for you, the reader, to help you understand what is happening.
     Feel free to delete them once you know what you're doing, but they should serve as a guide
